10367a678fd460054c300d82dbc99641402a3509
Alex Frezoul <frezoul.alex@gmail.com>
2020-09-29 16:36:33 +0200

    loading page done

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SplashScreen from "./src/screens/SplashScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This will be a fucking NATIVE INSTGRAM CLONE!</Text>
-      <StatusBar style="auto" />
+      <SplashScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+    marginTop: 20,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
